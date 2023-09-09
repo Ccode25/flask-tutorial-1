7e71483f8c9beba72bb55807c683ebff885b6328
Ccode25 <140866856+Ccode25@users.noreply.github.com>
2023-09-09 08:20:23 +0000

    apply bootstrap5 and create a html file to render data to HTML

--- app.py
+++ app.py
@@ -2,9 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Manila, Philippines',
+    'salary': 'PHP80,3000'
+  },
+  {
+    'id': 2,
+    'title': 'QA Tester',
+    'location': 'CALABARZON, Philippines',
+    'salary': 'PHP40,3000'
+  },
+  {
+    'id': 1,
+    'title': 'Software Develover',
+    'location': 'Manila, Philippines',
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',
+                         jobs = JOBS)
 
 if __name__ == "__main__":
   app.run(host = '0.0.0.0',debug=True)
